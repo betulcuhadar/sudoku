378629133f4022019b559840c78567f1ea887b30
Betül Çuhadar <102623204+betulcuhadar@users.noreply.github.com>
2022-09-20 14:38:02 +0300

    second commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server running");
 });
 
